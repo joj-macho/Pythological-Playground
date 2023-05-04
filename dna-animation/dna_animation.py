330146ec0c5a9d053180639a2bf50abfe631c496
Joj Macho <zingakay@gmail.com>
2023-05-04 16:37:05 +0200

    dna animation program

---
+++ dna-animation/dna_animation.py
@@ -0,0 +1,61 @@
+import random
+import sys
+import time
+
+# DNA Strand
+STRAND = [
+
+    '         ##',
+    '        #{}-{}#',
+    '       #{}---{}#',
+    '      #{}-----{}#',
+    '     #{}------{}#',
+    '    #{}------{}#',
+    '    #{}-----{}#',
+    '     #{}---{}#',
+    '     #{}-{}#',
+    '      ##',
+    '     #{}-{}#',
+    '     #{}---{}#',
+    '    #{}-----{}#',
+    '    #{}------{}#',
+    '     #{}------{}#',
+    '      #{}-----{}#',
+    '       #{}---{}#',
+    '        #{}-{}#']
+
+
+def main():
+    '''This is the main function'''
+
+    print('\nDNA Animation\n')
+
+    try:
+        input('Press Enter To Continue...')
+
+        strand_i = 0
+        while True:
+            # Row increment
+            strand_i += 1
+            if strand_i == len(STRAND):
+                strand_i = 0
+            # Indexes 0 and 9 have no nucleotides
+            if strand_i == 0 or strand_i == 9:
+                print(STRAND[strand_i])
+                continue
+
+            # Random Nucleotide pairs
+            nucleotide_pairs = [('A', 'T'), ('T', 'A'), ('C', 'G'), ('G', 'C')]
+            left_nucleotide, right_nucleotide = random.choice(nucleotide_pairs)
+
+            # Rows
+            print(STRAND[strand_i].format(left_nucleotide, right_nucleotide))
+            time.sleep(0.15)
+
+    except KeyboardInterrupt:
+        print('Goodbye')
+        sys.exit()
+
+
+if __name__ == '__main__':
+    main()

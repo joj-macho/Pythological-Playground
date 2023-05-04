e25a0f3baf6890454723ebd9e4955c3f3d7ba186
Joj Macho <zingakay@gmail.com>
2023-05-04 16:21:46 +0200

    digital clock program

---
+++ digital-clock-cli/digital_clock.py
@@ -0,0 +1,48 @@
+import sys
+import time
+import seven_segment
+
+
+def main():
+    print('\nDigital Timer.\n')
+
+    try:
+        while True:
+            # Get time from computer
+            current_time = time.localtime()
+            hours = str(current_time.tm_hour % 24)
+            minutes = str(current_time.tm_min)
+            seconds = str(current_time.tm_sec)
+
+            # Get digit strings from seven_segments
+            hour_digit = seven_segments.generate_seven_segments(hours, 2)
+            hour_top_row, hour_middle_row, hour_bottom_row = hour_digit.splitlines()
+
+            minute_digit = seven_segments.generate_seven_segments(minutes, 2)
+            minute_top_row, minute_middle_row, minute_bottom_row = minute_digit.splitlines()
+
+            sec_digit = seven_segments.generate_seven_segments(seconds, 2)
+            sec_top_row, sec_middle_row, sec_bottom_row = sec_digit.splitlines()
+
+            # Display the digits:
+            print(hour_top_row + '     ' + minute_top_row + '     ' + sec_top_row)
+            print(hour_middle_row + '  *  ' +
+                  minute_middle_row + '  *  ' + sec_middle_row)
+            print(hour_bottom_row + '  *  ' +
+                  minute_bottom_row + '  *  ' + sec_bottom_row)
+
+            print()
+            print('Press Ctrl-C to quit.')
+
+            # Loop through seconds
+            while True:
+                time.sleep(0.01)
+                if time.localtime().tm_sec != current_time.tm_sec:
+                    break
+
+    except KeyboardInterrupt:
+        sys.exit()
+
+
+if __name__ == '__main__':
+    main()
---
+++ digital-clock-cli/seven_segment.py
@@ -0,0 +1,84 @@
+
+def main():
+    print('\nSeven-Segment Number Display\n')
+    # Check to see if valid number entered.
+    while True:
+        response = input('Enter number to show:\n> ')
+        if not response.isdecimal():
+            continue
+        else:
+            response = int(response)
+            break
+
+    print(generate_seven_segments(response))
+    print()
+
+
+def generate_seven_segments(number, minWidth=0):
+    '''This function takes the number and the 0-padding as an input and returns a seven segment display string of the number'''
+
+    number = str(number).zfill(minWidth)
+    # print(number)
+    rows = ['', '', '']
+    for i, numeral in enumerate(number):
+        if numeral == '.':
+            rows[0] += ' '
+            rows[1] += ' '
+            rows[2] += '.'
+            continue
+
+        elif numeral == '-':
+            rows[0] += '    '
+            rows[1] += ' __ '
+            rows[2] += '    '
+        elif numeral == '0':
+            rows[0] += ' __ '
+            rows[1] += '|  |'
+            rows[2] += '|__|'
+        elif numeral == '1':
+            rows[0] += '    '
+            rows[1] += '   |'
+            rows[2] += '   |'
+        elif numeral == '2':
+            rows[0] += ' __ '
+            rows[1] += ' __|'
+            rows[2] += '|__ '
+        elif numeral == '3':
+            rows[0] += ' __ '
+            rows[1] += ' __|'
+            rows[2] += ' __|'
+        elif numeral == '4':
+            rows[0] += '    '
+            rows[1] += '|__|'
+            rows[2] += '   |'
+        elif numeral == '5':
+            rows[0] += ' __ '
+            rows[1] += '|__ '
+            rows[2] += ' __|'
+        elif numeral == '6':
+            rows[0] += ' __ '
+            rows[1] += '|__ '
+            rows[2] += '|__|'
+        elif numeral == '7':
+            rows[0] += ' __ '
+            rows[1] += '   |'
+            rows[2] += '   |'
+        elif numeral == '8':
+            rows[0] += ' __ '
+            rows[1] += '|__|'
+            rows[2] += '|__|'
+        elif numeral == '9':
+            rows[0] += ' __ '
+            rows[1] += '|__|'
+            rows[2] += ' __|'
+
+        if i != len(number) - 1 and number[i + 1] != '.':
+            rows[0] += ' '
+            rows[1] += ' '
+            rows[2] += ' '
+
+    return '\n'.join(rows)
+
+
+if __name__ == '__main__':
+    main()

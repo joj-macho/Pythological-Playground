4ea878ad162de850c71317e77c120313e0563ee2
Joj Macho <zingakay@gmail.com>
2023-05-22 15:33:34 +0200

    indentation animation program

---
+++ indentation-animation/indentation_animation.py
@@ -0,0 +1,40 @@
+import time
+import sys
+
+def main():
+    '''This is the main program that animates an indentation effect with asterisks.'''
+
+    print('\nIndentation Animation.\n')
+    
+    # Spaces to indent.
+    indent = 0 
+    # Increase indentation or not?
+    is_indent_increasing = True  
+    # Pause duration in seconds.
+    sleep_duration = 0.1  
+
+    try:
+        # The main program loop.
+        while True:  
+            print(' ' * indent, end='')
+            print('********')
+            time.sleep(sleep_duration)
+
+            if is_indent_increasing:
+                # Increase the number of spaces:
+                indent += 1
+                if indent == 20:
+                    # Reverse direction:
+                    is_indent_increasing = False
+            else:
+                # Decrease the number of spaces:
+                indent -= 1
+                if indent == 0:
+                    # Reverse direction:
+                    is_indent_increasing = True
+
+    except KeyboardInterrupt:
+        sys.exit()
+
+if __name__ == '__main__':
+    main()

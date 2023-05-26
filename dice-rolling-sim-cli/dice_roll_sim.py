fb508edd676b6f03db2ad691d628d8666b8eba27
Joj Macho <zingakay@gmail.com>
2023-05-26 09:45:42 +0200

    added more functions to modularize the program

--- dice-rolling-sim-cli/dice_roll_sim.py
+++ dice-rolling-sim-cli/dice_roll_sim.py
@@ -48,54 +48,71 @@ DICE_FACES = {
 
 # The height of each dice face in rows
 DICE_HEIGHT = len(DICE_FACES[1])
+# Maximum allowed dice
+MAX_DICE = 6
 
 
 def main():
-    '''
-    The main driver code
-    '''
-    print('Dice Rolling Simulator\n')
+    '''This is the main function. It prompts the user to input the number of dice to roll, generates random dice roll results, and displays the ASCII representation of the rolled dice faces.'''
 
-    # Prompt user to choose number of dice to roll
+    print('\nDice Rolling Simulator\n')
+
     while True:
-        response = input('How many dice do you want to roll (max 6 dice)?\n> ')
-        
-        if response.isdigit() and 0 < int(response) <= 6:
-            num_of_dice = int(response)
-            break
-        else:
-            print('Enter a valid number of dice to roll!')
-    
-    # Roll the dice using the random module.
-    dice_roll_results = [random.randint(1, 6) for _ in range(num_of_dice)]
+        num_of_dice = get_num_of_dice()
+        dice_roll_results = roll_dice(num_of_dice)
 
-    # Display Dice Faces
-    show_dice_face = generate_dice_face(dice_roll_results)
-    print('RESULTS:')
-    print(show_dice_face)
+        print('RESULTS:')
+        show_dice_face = generate_dice_face(dice_roll_results)
+        print(show_dice_face)
+
+        if not play_again():
+            break
+
+
+def get_num_of_dice():
+    '''This function prompts user to choose the number of dice to roll.'''
+
+    while True:
+        try:
+            response = int(input(f'How many dice do you want to roll (max {MAX_DICE} dice)?\n> '))
+            if 0 < response <= MAX_DICE:
+                return response
+            print(f'Enter a valid number of dice to roll (1-{MAX_DICE})!')
+        except ValueError:
+            print('Enter a valid number!')
+
+
+def roll_dice(num_of_dice):
+    '''This function rolls the dice using the random module.'''
+
+    return [random.randint(1, 6) for _ in range(num_of_dice)]
 
 
 def generate_dice_face(dice_roll_results):
-    '''
-    This function returns the ASCII characters of a rolled die.
-    It takes a list of dice roll results as a parameter.
-    '''
-    # List of dice face characters
-    dice_face = [DICE_FACES[face] for face in dice_roll_results]
-    # print(diceFace)
-    
-    # List of dice face rows
-    dice_face_rows = []
-    for row in range(DICE_HEIGHT):
-        row_item = []
-        for die in dice_face:
-            row_item.append(die[row])
-        row_text = ' '.join(row_item)
-        dice_face_rows.append(row_text)
+    '''This function returns the ASCII characters of a rolled die.'''
 
+    try:
+        dice_face = [DICE_FACES[face] for face in dice_roll_results]
+    except KeyError:
+        return 'Invalid dice face encountered.'
+
+    dice_face_rows = [' '.join(die_row) for die_row in zip(*dice_face)]
     show_dice_face = '\n'.join(dice_face_rows)
 
     return show_dice_face
+
+
+def play_again():
+    '''This function prompts the user to play again.'''
+
+    while True:
+        response = input('Do you want to roll again? (y/n)\n> ').lower()
+        if response == 'y':
+            return True
+        elif response == 'n':
+            return False
+        else:
+            print('Enter either "y" or "n"!')
 
 
 if __name__ == '__main__':

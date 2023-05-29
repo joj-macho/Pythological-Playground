6c11ac814abda5a40dbbdd23c2d7624bf6fb67f8
Joj Macho <zingakay@gmail.com>
2023-05-29 15:12:27 +0200

    added skeleton program

--- maze-runner/maze_runner_2d.py
+++ maze-runner/maze_runner_2d.py
@@ -1,7 +1,58 @@
-'''Maze Runner 2D Program'''
+import sys
+import os
+
+# Constants and Configuration
+WALL = '#'
+EMPTY = ' '
+START = 'S'
+EXIT = 'E'
+
+PLAYER = '@'
+BLOCK = chr(9617) 
+
+WIDTH = 0
+HEIGHT = 0
+player_X = None
+player_Y = None
+exit_X = None
+exit_Y = None
+
 
 def main():
+    '''Main game loop.'''
+
     pass
+
+
+def display_maze(maze):
+    '''Display the maze.'''
+
+    pass
+
+
+def load_maze(filename):
+    '''Load the maze from a file.'''
+
+    pass
+
+
+def get_user_move():
+    '''Get user move (direction or quit).'''
+
+    pass
+
+
+def can_move_to(x, y):
+    '''Check if the player can move to the given position.'''
+
+    pass
+
+
+def move_player(move):
+    '''Move the player in the specified direction.'''
+
+    pass
+
 
 
 if __name__ == '__main__':
--- maze-runner/maze_runner_3d.py
+++ maze-runner/maze_runner_3d.py
@@ -1,6 +1,69 @@
-'''Maze Runner 3D Program'''
+import copy
+import sys
+import os
+
+
+# Constants and Configuration
+WALL = '#'
+EMPTY = ' '
+START = 'S'
+EXIT = 'E'
+BLOCK = chr(9617) 
+NORTH = 'NORTH'
+SOUTH = 'SOUTH'
+EAST = 'EAST'
+WEST = 'WEST'
+
+
+def wall_str_to_wall_dict(wall_string):
+    '''Takes a string representation of a wall drawing (like those in ALL_OPEN or CLOSED) and returns a representation in a dictionary with (x, y) tuples as keys and single-character strings of the character to draw at that x, y location.'''
+    pass
+
+
+def display_wall_dict(wall_dict):
+    '''Display a wall dictionary, as returned by wall_str_to_wall_dict(), on the screen.'''
+    pass
+
+
+def paste_wall_dict(src_wall_dict, dst_wall_dict, left, top):
+    '''Copy the wall representation dictionary in src_wall_dict on top of the one in
+    dst_wall_dict, offset to the position given by left, top.'''
+    pass
+
+
+def make_wall_dict(maze, player_X, player_Y, player_direction, exit_X, exit_Y):
+    '''From the player's position and direction in the maze (which has an exit at exit_X, exit_Y), create the wall representation dictionary by pasting wall dictionaries on top of ALL_OPEN, then return it.'''
+
+    pass
+
+
+def load_maze_file(filename):
+    '''Load the maze from a file and return the maze dictionary, player's starting position, exit position, and maze dimensions.'''
+
+    pass
+
+
+def get_user_move():
+    '''Get user input for the next move and return the move.'''
+
+    pass
+
+
+def move_player(move):
+    '''Move the player according to the specified move.'''
+
+    pass
+
+
+def play_game():
+    '''Start and play the Maze Runner game.'''
+
+    pass
+
 
 def main():
+    '''Main function of the program.'''
+
     pass
 
 

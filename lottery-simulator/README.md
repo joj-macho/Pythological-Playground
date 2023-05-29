3f80f5f977be6c6754ba5a88cca5e0b236a337dc
Joj Macho <zingakay@gmail.com>
2023-05-29 14:27:08 +0200

    lottery simulator program

--- lottery-simulator/README.md
+++ lottery-simulator/README.md
@@ -2,11 +2,96 @@
 
 ## Description
 
+This program is a Powerball Lottery simulator. It allows users to experience the excitement of playing the Powerball Lottery without spending any money.
+
 ## How it Works
+
+- The program first imports the `random` module. The `random` module is essential in creating a realistic simulation of the lottery draw by introducing randomness in the selection of winning numbers and the powerball number.
+
+- The program starts by defining constants for the ticket cost (`TICKET_COST`), the jackpot amount (`JACKPOT_AMOUNT`), and the maximum number of plays (`MAX_PLAYS`).
+
+- The `main()` function is defined and calle, this function displays an introductory message to welcome the user and explain the purpose of the simulation. Inside the `main()` function, there is a while loop that allows the user to play the game multiple times.
+
+- The `get_numbers()` function prompts the user to enter five different numbers from 1 to 69. It validates the input to ensure it meets the required criteria.
+
+- The `get_powerball()` function prompts the user to enter the powerball number from 1 to 26. It also validates the input.
+
+- The `get_num_of_plays()` function prompts the user to enter the number of times they want to play. It ensures that the input is within the valid range.
+
+- The `simulate_lottery()` function takes the user's chosen numbers, powerball number, and the number of plays as input. It simulates the lottery draw by generating random winning numbers and a powerball number. It checks if the user's numbers match the winning numbers and powerball number to determine if they have won.
+
+- After each simulation, the user is given the option to play again. If the user chooses to continue, the game repeats. Otherwise, the program ends with a thank-you message.
+
 
 ## Program Input & Output
 
 When you run the program `lottery_sim.py`, the output will look like this;
 
 ```
+
+Welcome to the Powerball Lottery Simulator!
+
+Experience the excitement of the Powerball Lottery without spending a dime.
+
+Each Powerball lottery ticket typically costs $2, and the current jackpot stands at $500000000.
+However, with odds of 1 in 292,201,338, winning the jackpot is extremely unlikely.
+
+This simulation allows you to enjoy the thrill of playing without the financial risk.
+
+Enter 5 different numbers from 1 to 69, separated by spaces:
+> 1 2 3 4 5
+Enter the powerball number from 1 to 26:
+> 6
+How many times do you want to play? (Max: 1000000):
+> 20
+
+It costs $ 40 to play 20 times, but don't
+worry. I'm sure you'll win it all back.
+Press Enter to start...
+
+The winning numbers are: 62 38 46 30 50 and 3You lost.
+
+The winning numbers are: 19 53 7 63 49 and 2You lost.
+
+The winning numbers are: 19 9 57 33 29 and 6You lost.
+
+The winning numbers are: 67 37 19 69 29 and 19You lost.
+
+The winning numbers are: 32 17 67 37 61 and 21You lost.
+
+The winning numbers are: 31 25 69 40 33 and 10You lost.
+
+The winning numbers are: 11 5 2 15 17 and 2You lost.
+
+The winning numbers are: 20 69 40 50 56 and 10You lost.
+
+The winning numbers are: 30 17 43 57 6 and 17You lost.
+
+The winning numbers are: 12 16 4 39 51 and 4You lost.
+
+The winning numbers are: 4 33 30 62 31 and 3You lost.
+
+The winning numbers are: 25 10 52 20 63 and 14You lost.
+
+The winning numbers are: 34 30 12 59 21 and 16You lost.
+
+The winning numbers are: 59 68 55 38 44 and 26You lost.
+
+The winning numbers are: 6 38 67 26 5 and 10You lost.
+
+The winning numbers are: 28 48 21 41 58 and 25You lost.
+
+The winning numbers are: 23 29 59 11 13 and 18You lost.
+
+The winning numbers are: 58 30 36 10 46 and 15You lost.
+
+The winning numbers are: 29 7 28 40 15 and 2You lost.
+
+The winning numbers are: 37 25 69 64 49 and 14You lost.
+
+You have wasted $ 40
+
+Do you want to play again? Enter (y)es/(n)o:
+> n
+Thanks for playing!
 ```
--- lottery-simulator/lottery_sim.py
+++ lottery-simulator/lottery_sim.py
@@ -1,8 +1,120 @@
-'''Lottery Simulator Program'''
+import random
+
+TICKET_COST = 2
+JACKPOT_AMOUNT = 500000000
+MAX_PLAYS = 1000000
 
 def main():
-    pass
+    '''Main function that simulates the Powerball Lottery game.'''
+    
+    print('\nWelcome to the Powerball Lottery Simulator!\n')
+    print('Experience the excitement of the Powerball Lottery without spending a dime.\n')
+    print(f'Each Powerball lottery ticket typically costs $2, and the current jackpot stands at ${JACKPOT_AMOUNT}.')
+    print('However, with odds of 1 in 292,201,338, winning the jackpot is extremely unlikely.\n')
+    print('This simulation allows you to enjoy the thrill of playing without the financial risk.\n')
 
 
+    while True:
+        numbers = get_numbers()
+        powerball = get_powerball()
+        num_of_plays = get_num_of_plays()
+        simulate_lottery(numbers, powerball, num_of_plays)
+
+        replay = input('\nDo you want to play again? Enter (y)es/(n)o:\n> ')
+        if replay.lower() != 'yes':
+            break
+
+    print('Thanks for playing!')
+
+def get_numbers():
+    '''Prompt the user to enter 5 different numbers from 1 to 69.'''
+    
+    while True:
+        response = input('Enter 5 different numbers from 1 to 69, separated by spaces:\n> ')
+        numbers = response.split()
+
+        if len(numbers) != 5:
+            print('Please enter 5 numbers.')
+            continue
+
+        try:
+            numbers = [int(num) for num in numbers]
+        except ValueError:
+            print('Please enter numbers only.')
+            continue
+
+        if not all(1 <= num <= 69 for num in numbers):
+            print('The numbers must be between 1 and 69.')
+            continue
+
+        if len(set(numbers)) != 5:
+            print('You must enter 5 different numbers.')
+            continue
+
+        return numbers
+
+def get_powerball():
+    '''Prompt the user to enter the powerball number from 1 to 26.'''
+    
+    while True:
+        response = input('Enter the powerball number from 1 to 26:\n> ')
+
+        try:
+            powerball = int(response)
+        except ValueError:
+            print('Please enter a number.')
+            continue
+
+        if not (1 <= powerball <= 26):
+            print('The powerball number must be between 1 and 26.')
+            continue
+
+        return powerball
+
+def get_num_of_plays():
+    '''Prompt the user to enter the number of times they want to play.'''
+    
+    while True:
+        response = input('How many times do you want to play? (Max: 1000000):\n> ')
+
+        try:
+            num_of_plays = int(response)
+        except ValueError:
+            print('Please enter a number.')
+            continue
+
+        if not (1 <= num_of_plays <= MAX_PLAYS):
+            print('You can play between 1 and', MAX_PLAYS, 'times.')
+            continue
+
+        return num_of_plays
+
+def simulate_lottery(numbers, powerball, num_of_plays):
+    '''Simulate the lottery draw and check if the user has won.'''
+    
+    price = TICKET_COST * num_of_plays
+    print('\nIt costs $', price, 'to play', num_of_plays, 'times, but don\'t')
+    print('worry. I\'m sure you\'ll win it all back.')
+    input('Press Enter to start...')
+
+    possibleNumbers = list(range(1, 70))
+    for _ in range(num_of_plays):
+        random.shuffle(possibleNumbers)
+        winning_numbers = possibleNumbers[:5]
+        winning_powerball = random.randint(1, 26)
+
+        print('\nThe winning numbers are:', end=' ')
+        all_winning_nums = ' '.join(str(num) for num in winning_numbers)
+        print(all_winning_nums, 'and', winning_powerball, end='')
+
+        if set(numbers) == set(winning_numbers) and powerball == winning_powerball:
+            print('\nYou have won the Powerball Lottery! Congratulations,')
+            print('you would be a billionaire if this was real!')
+            break
+        else:
+            print('You lost.')
+
+    print('\nYou have wasted $', price)
+    
 if __name__ == '__main__':
     main()

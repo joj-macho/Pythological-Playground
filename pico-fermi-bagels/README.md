1f2d0466aff2c88f879bd1d5a9fb965cc3070a09
Joj Macho <zingakay@gmail.com>
2023-09-02 22:31:26 +0200

    Fix main function, update comments and readme file

--- pico-fermi-bagels/README.md
+++ pico-fermi-bagels/README.md
@@ -9,9 +9,7 @@ The program is a number guessing game called "Pico Fermi Bagels" or simply "Bage
 
 - The program begins by importing the `random` module which is used to generate random numbers and make the game more unpredictable and challenging. The program then defines two constants: `NUM_DIGITS`, which represents the number of digits in the secret number, and `NUM_GUESSES`, which determines the maximum number of guesses allowed.
 
-- The `main()` function is defined. This function prints the game instructions and enters a loop that allows the player to play multiple games.
-
-- Inside the `play_game()` function, a new secret number is generated using the `generate_secret_number()` function, which selects `NUM_DIGITS` random digits without repetition.
+- The `main()` function starts by printing the game instructions and enters a loop that allows the player to play multiple games. A new secret number is generated using the `generate_secret_number()` function, which selects `NUM_DIGITS` random digits without repetition.
 
 - The player is informed about the number of digits and the number of guesses they have. The player's guesses are collected using the `get_valid_guess()` function, which prompts the user to enter a valid `NUM_DIGITS`-digit number.
 
@@ -20,16 +18,6 @@ The program is a number guessing game called "Pico Fermi Bagels" or simply "Bage
 - The game continues until the player guesses the correct number, runs out of guesses, or decides not to play again. If the player guesses correctly, a congratulatory message is displayed. If the player runs out of guesses, the correct answer is revealed.
 
 - The `play_again()` function prompts the player to choose whether they want to play another game or quit. It validates the input and returns a Boolean value indicating the player's decision.
-
-- The `generateSecretNumber()` function generates a random non-repeating string of NUM_DIGITS numbers using the `random.shuffle()` function.
-
-
-- The `generateClues()` function takes the user's guess and the secret number as input and returns a string of clues. The function first checks if the guess is correct and returns a message if it is. If the guess is not correct, the function generates clues based on the user's guess. If a digit in the guess is in the correct position, the function adds "Fermi" to the clues list. If a digit in the guess is in the secret number but in the wrong position, the function adds "Pico" to the clues list. If none of the digits in the guess are in the secret number, the function returns "Bagels". The clues are then sorted and returned as a string.
-
-
-- The `main()` function uses a while loop to keep the game running until the user chooses to quit. In each iteration of the loop, the program prompts the user to guess the secret number and provides clues based on the guess. The loop continues until the user guesses the correct number or runs out of guesses.
-
-- When the game is over, the `main()` function prompts the user to play again or quit. If the user chooses to play again, the while loop in `main()` restarts the game. If the user chooses to quit, the program exits.
 
 
 ## Program Input & Output
--- pico-fermi-bagels/pico_fermi_bagels.py
+++ pico-fermi-bagels/pico_fermi_bagels.py
@@ -5,7 +5,7 @@ NUM_DIGITS = 3
 NUM_GUESSES = 10
 
 def main():
-    '''This is the main function'''
+    '''Main function to handle the logic of the game.'''
 
     print('''
 Pico Fermi Bagels!
@@ -16,38 +16,47 @@ Pico -- One digit is correct, but in the wrong position.
 Fermi --- One digit is correct, and in the right position.
 Bagels --- No digit is correct.
     ''')
+
     while True:
-        play_game()
+        # Generate a secret number.
+        secret_number = generate_secret_number()
+        print(f'\nI thought of a {NUM_DIGITS}-digit number. Can you guess it?\nYou have {NUM_GUESSES} tries to guess the number correctly!\n')
+        # print(secret_number)
+        
+        number_of_guesses = 1
+        while number_of_guesses <= NUM_GUESSES:
+            # Get a valid guess from the player
+            guess = get_valid_guess()
+
+            # Generate clues based on the player's guess and the secret number
+            clues = generate_clues(guess, secret_number)
+            print(clues)
+            number_of_guesses += 1
+
+            # Check if the player guessed the correct number
+            if guess == secret_number:
+                print('Congratulations! You guessed the correct number!')
+                break
+
+            # When you run out of guesses
+            if number_of_guesses > NUM_GUESSES:
+                print(f'You ran out of guesses. The correct answer was {secret_number}')
+                
         if not play_again():
             break
 
-def play_game():
-    '''This function handles the logic of playing a single game. '''
-
-    secret_number = generate_secret_number()
-    print(f'\nI thought of a {NUM_DIGITS}-digit number. Can you guess it?\nYou have {NUM_GUESSES} tries to guess the number correctly!\n')
-    # print(secret_number)
-    
-    number_of_guesses = 1
-    while number_of_guesses <= NUM_GUESSES:
-        guess = get_valid_guess()
-        clues = generate_clues(guess, secret_number)
-        print(clues)
-        number_of_guesses += 1
-        if guess == secret_number:
-            print('Congratulations! You guessed the correct number!')
-            break
-        if number_of_guesses > NUM_GUESSES:
-            print(f'You ran out of guesses. The correct answer was {secret_number}')
 
 def generate_secret_number():
-    '''This function returns a string of random NUM_DIGITS numbers.'''
+    '''Generate and return a string of random NUM_DIGITS numbers.'''
 
+    # Generate unique digits
     numbers = random.sample('0123456789', NUM_DIGITS)
+
     return ''.join(numbers)
 
+
 def get_valid_guess():
-    '''This function prompts the user to enter a valid 3-digit number.'''
+    '''Prompt the user to enter a valid 3-digit number.'''
 
     while True:
         guess = input(f'Guess: ')
@@ -56,8 +65,9 @@ def get_valid_guess():
         else:
             print(f'Enter a valid {NUM_DIGITS}-digit number.')
 
+
 def generate_clues(guess, secret_number):
-    '''This function takes the user's guess and the secret number as input and returns a string of clues.'''
+    '''Generate and return a string of clues based on the user's guess and the secret number.'''
 
     if guess == secret_number:
         return 'You guessed CORRECTLY!'
@@ -75,8 +85,10 @@ def generate_clues(guess, secret_number):
         clues.sort()
         return ' '.join(clues)
 
+
 def play_again():
-    '''This functin prompts the user to play again or quit.'''
+    '''Prompt the user to play again or quit.'''
+
     while True:
         response = input('Do you want to play again? (y)es or (n)o:\n> ').lower()
         if response.startswith('y'):
@@ -86,6 +98,6 @@ def play_again():
         else:
             print('Invalid input. Please enter (y)es or (n)o.')
 
+
 if __name__ == '__main__':
     main()
-
